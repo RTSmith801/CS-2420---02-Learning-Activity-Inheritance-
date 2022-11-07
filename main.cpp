6d6f2a3c29bc5c4f4cb301d73affc18821c9c16e
Wreckeshey <RTSmith801@gmail.com>
2022-11-06 17:11:49 -0700

    initial commit for cLion

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file: Inheritance.cpp
+ * @assignment_name:  02 Learning Activity Inheritance
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 11/06/2022 - started : 11/06/2022 - last update
+ * @brief: Using inheritance to create a simple battle program
+ *
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
